0d3d36729a0ec92128b6d9e571ead7918efb5915
BegoCaZa <begoxcz@hotmail.com>
2025-03-17 11:51:40 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+// let user
+// user='pepe'
+
+// let numberA = 89
+
+// const numberB= 64
+
+// console.log(numberA)
+// numberA= 12
+
+// console.log(numberA)
+
+// function printOne(){
+//     console.log(1);
+// }
+
+// function printTwo(){
+//     console.log(2);
+// }
+
+// function printThree(){
+//     console.log(3);
+// }
+
+// printOne();
+// printTwo();
+// printThree();
+
+function printNumber(cosa){
+    console.log(cosa +1);
+}
+
+// esto lo vuelve palabra
+printNumber('1'); 
+printNumber(2);
+printNumber(3);
+
+// operaciones
+
+function printOperation(numberA,numberB) {
+    const result= numberA + numberB;
+    console.log("El resultado de la suma es " + result);
+}
+
+printOperation(7,9);
+printOperation(8,59);
